d166e4dd8d00bd1bade977ad55f015928f899260
l1753531985 <l1753531985@gmail.com>
2022-04-08 06:11:14 -0400

    version1.1 of ls1

---
+++ fileinfo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/stat.h>
+
+void show_modtime(long time)
+{
+	printf("	modtime: %12.12s\n", ctime(&time)+4);
+}
+
+void show_stat_info(char* fname, struct stat* buf)
+{
+	printf("	mode: %o\n", buf->st_mode);
+	printf("	links: %d\n", buf->st_nlink);
+	printf("	user: %d\n", buf->st_uid);
+	printf("	group: %d\n", buf->st_gid);
+	printf("	size: %d\n", buf->st_size);
+	show_modtime(buf->st_mtime);
+	printf("	name: %s\n", fname);
+}
+
+int main(int ac, char* av[])
+{
+	struct stat info;
+	if (ac > 1)
+		if (stat(av[1], &info) == 0)
+		{
+			show_stat_info(av[1], &info);
+			return 0;
+		}
+		else
+			perror(av[1]);
+	return 1;
+}
